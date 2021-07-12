7106c877094847a2a315d44c51e6d16cbe5ea652
evandonova <evelina_ga@abv.bg>
2021-07-12 13:34:39 +0300

    Added initial project

---
+++ SharedGoals/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedGoals.Models;
+using System.Diagnostics;
+
+namespace SharedGoals.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
