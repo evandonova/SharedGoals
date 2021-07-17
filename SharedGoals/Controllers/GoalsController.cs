33cef2b1c6955f35514c70b78da67ae9600e0cb7
evandonova <evelina_ga@abv.bg>
2021-07-17 15:55:45 +0300

    Added functionalities and changed html in views

--- SharedGoals/Controllers/GoalsController.cs
+++ SharedGoals/Controllers/GoalsController.cs
@@ -34,6 +34,12 @@ namespace SharedGoals.Controllers
         public IActionResult Details(int id)
         {
             var goal = this.dbContext.Goals.FirstOrDefault(g => g.Id == id);
+
+            if (goal == null)
+            {
+                return View();
+            }
+
             var goalData = new GoalDetailsViewModel()
             {
                 Id = goal.Id,
@@ -45,11 +51,6 @@ namespace SharedGoals.Controllers
                 Tag = this.dbContext.Tags.FirstOrDefault(t => t.Id == goal.TagId).Name,
                 Owner = this.User.Identity.Name
             };
-
-            if(goal == null)
-            {
-                return View();
-            }
 
             return View(goalData);
         }
@@ -92,6 +93,97 @@ namespace SharedGoals.Controllers
             this.dbContext.SaveChanges();
 
             return Redirect("/");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var goal = this.dbContext.Goals.FirstOrDefault(g => g.Id == id);
+
+            if (goal == null)
+            {
+                return this.View();
+            }
+
+            var tagName = this.dbContext.Tags.FirstOrDefault(t => t.Id == goal.TagId).Name;
+            GoalListingViewModel model = new GoalListingViewModel()
+            {
+                Id = goal.Id,
+                Name = goal.Name,
+                ProgressInPercents = goal.ProgressInPercents.ToString(),
+                DueDate = goal.DueDate.ToString(),
+                Tag = tagName
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(GoalListingViewModel goalModel)
+        {
+            var goal = this.dbContext.Goals.FirstOrDefault(g => g.Id == goalModel.Id);
+            if (goal == null)
+            {
+                return this.View();
+            }
+
+            dbContext.Goals.Remove(goal);
+            dbContext.SaveChanges();
+            return this.RedirectToAction("All");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var goal = this.dbContext.Goals.FirstOrDefault(g => g.Id == id);
+
+            if (goal == null)
+            {
+                return this.View();
+            }
+
+            CreateGoalFormModel model = new CreateGoalFormModel()
+            {
+                Name = goal.Name,
+                Description = goal.Description,
+                DueDate = goal.DueDate,
+                TagId = goal.TagId,
+                Tags = GetGoalTags()
+            };
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, CreateGoalFormModel bindingModel)
+        {
+            var goal = this.dbContext.Goals.FirstOrDefault(g => g.Id == id);
+            if (goal == null)
+            {
+                return this.View();
+            }
+
+            if (!this.dbContext.Tags.Any(c => c.Id == bindingModel.TagId))
+            {
+                this.ModelState.AddModelError(nameof(bindingModel.TagId), "Tag does not exist.");
+            }
+
+            if (bindingModel.DueDate <= DateTime.UtcNow || bindingModel.DueDate.Year > 2100)
+            {
+                this.ModelState.AddModelError(nameof(bindingModel.DueDate), "Due Date must be in the future and before 2100 year.");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                bindingModel.Tags = GetGoalTags();
+                return this.View(bindingModel); 
+            }
+
+            goal.Name = bindingModel.Name;
+            goal.Description = bindingModel.Description;
+            goal.DueDate = bindingModel.DueDate;
+            goal.TagId = bindingModel.TagId;
+
+            dbContext.SaveChanges();
+            return this.RedirectToAction("All");
         }
 
         private IEnumerable<GoalTagViewModel> GetGoalTags()
